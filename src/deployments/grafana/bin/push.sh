506e7488de430573b6c6c86583603b8e2af2ba27
InvisiBug <matthewjkavanagh@gmail.com>
2021-09-12 02:30:49 +0100

    Cluster mostly working, just need to figure out registry

--- src/deployments/grafana/bin/push.sh
+++ src/deployments/grafana/bin/push.sh
@@ -6,4 +6,5 @@
 # docker tag grafana-interconnector 192.168.1.42:5000/grafana-interconnector && \
 # docker push 192.168.1.42:5000/grafana-interconnector 
 
-yarn install && yarn build && docker build -f ./Dockerfile.kube -t grafana-interconnector . && docker tag grafana-interconnector 192.168.1.42:5000/grafana-interconnector && docker push 192.168.1.42:5000/grafana-interconnector 
+REGISTRY_IP=192.168.1.62
+yarn install && yarn build && docker build -f ./Dockerfile.kube -t grafana-interconnector . && docker tag grafana-interconnector $REGISTRY_IP:5000/grafana-interconnector && docker push $REGISTRY_IP:5000/grafana-interconnector 
