dc517961e1480454c16fb665ec5b27ed8e08be3e
InvisiBug <matthewjkavanagh@gmail.com>
2021-09-16 17:20:34 +0100

    Added cluster info to grafana

--- src/deployments/grafana/bin/push.sh
+++ src/deployments/grafana/bin/push.sh
@@ -6,5 +6,5 @@
 # docker tag grafana-interconnector 192.168.1.42:5000/grafana-interconnector && \
 # docker push 192.168.1.42:5000/grafana-interconnector 
 
-REGISTRY_IP=192.168.1.62
+REGISTRY_IP=192.168.1.61
 yarn install && yarn build && docker build -f ./Dockerfile.kube -t grafana-interconnector . && docker tag grafana-interconnector $REGISTRY_IP:5000/grafana-interconnector && docker push $REGISTRY_IP:5000/grafana-interconnector 
