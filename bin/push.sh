1f71a25bb722be5659471ec4ca7cdc20855d2d19
InvisiBug <matthewjkavanagh@gmail.com>
2022-12-03 18:31:06 +0000

    Added new radiators

--- bin/push.sh
+++ bin/push.sh
@@ -7,4 +7,8 @@
 # docker push 192.168.1.42:5000/grafana-interconnector 
 
 REGISTRY_IP=192.168.1.61:5000
-yarn install && yarn build && docker build -f ./Dockerfile.kube -t grafana-interconnector . && docker tag grafana-interconnector $REGISTRY_IP/grafana-interconnector && docker push $REGISTRY_IP/grafana-interconnector 
+yarn install && \
+yarn build && \
+docker build -f ./Dockerfile.kube -t grafana-interconnector . && \
+docker tag grafana-interconnector $REGISTRY_IP/grafana-interconnector && \
+docker push $REGISTRY_IP/grafana-interconnector
